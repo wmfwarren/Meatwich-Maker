a7469b5d8932f5fe9af0452a28d12ec751a657b5
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-17 09:22:06 -0500

    Began work on SandwichDriver.js

--- js/DOMHandler.js
+++ js/DOMHandler.js
@@ -7,7 +7,7 @@ var DOMHandler = (function(){
 	allParts.push(Sandwich.getCheeses());
 	allParts.push(Sandwich.getCondiments());
 
-	for (let i = 0; i < allParts.length; i++){
+	for (let i = 0; i < allParts.length; i++){ //this loops through the array of part lists 
 		currentPart = allParts[i];
 		let currentNewDiv = document.createElement("div");
 		let newDivID = document.createAttribute("id");
@@ -15,10 +15,18 @@ var DOMHandler = (function(){
 		currentNewDiv.setAttributeNode(newDivID);
 		document.getElementById("pickers").appendChild(currentNewDiv);
 
+
 		let currentNewSelect = document.createElement("select");
 		let newSelectClass = document.createAttribute("class");
 		newSelectClass.value = "col-xs-2";
 		currentNewSelect.setAttributeNode(newSelectClass);
+
+		if( i === 0 ){
+			let firstSelectOffset = document.createAttribute("class");
+			firstSelectOffset.value = "col-xs-offset-1 col-xs-2";
+			currentNewSelect.setAttributeNode(firstSelectOffset);
+		}
+		
 		currentNewDiv.appendChild(currentNewSelect);
 
 		for(let j = 0; j < currentPart.length; j++){
@@ -28,17 +36,18 @@ var DOMHandler = (function(){
 
 			let newOption = document.createElement("option");
 			let newOptionValue = document.createAttribute("value");
-			newOptionValue.value = currentItem;
+			newOptionValue.value = currentItem; //add the current item to the current dropdown list
 			newOption.innerText = `${currentItem}`;
 
 			newOption.setAttributeNode(newOptionValue);
 			currentNewSelect.appendChild(newOption);
-
-
-
-
 		}
 	}
 
+	for (let i = 0; i < partTypes.length; i++){
+		let currentDropdown = document.getElementById(`${partTypes[i]}`);
+
+		currentDropdown.addEventListener("change", Sandwich.getItem);
+	}
 
 })();
--- js/sandwichDriver.js
+++ js/sandwichDriver.js
@@ -1,5 +1,47 @@
 var Sandwich = (function(sandwich){
-	totalPrice = 0.00;
+	var totalPrice = 0.00;
+
+	sandwich.getPrice = function(){
+		return totalPrice;
+	};
+
+	sandwich.getItem = function(event){
+		var selectedItem = event.target.value;
+		var targetArrayName = event.currentTarget.id;
+		var targetArray = null;
+
+		switch (targetArrayName) {
+			case "Meats": 
+				targetArray = Sandwich.getMeats();
+				break;
+			case "Veggies":
+				targetArray = Sandwich.getVeggies();
+				break;
+			case "Breads":
+				targetArray = Sandwich.getBreads();
+				break;
+			case "Cheeses":
+				targetArray = Sandwich.getCheeses();
+				break;
+			case "Condiments":
+				targetArray = Sandwich.getCondiments();
+				break;
+		}
+
+		displayItem(selectedItem, targetArray);
+
+	};
+
+	sandwich.displayItem = function (item, array){
+		for (let i = 0; i < array.length; i ++){
+			if (array[i].item = item){
+				totalPrice += array[i].price;
+
+				
+
+			}
+		}
+	};
 
 	return sandwich;
 })(Sandwich || {});
